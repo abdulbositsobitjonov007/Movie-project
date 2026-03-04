a734e1c297a00589d6280767f1afbb3befa6d95e
abdulbositsobitjonov007 <sobitjonovabdulbosit91@gmail.com>
2026-03-04 18:58:28 +0500

    deleting button play in swiper-slide-prev

--- components/BannerMovie.tsx
+++ components/BannerMovie.tsx
@@ -7,7 +7,7 @@ function BannerMovie() {
         <Image width={550} height={500} className="w-full h-full object-cover" quality={100} src={"/movieBanner_LE_upscale_prime.jpg"} alt="" />
       </div>
       <div className="absolute bottom-15 left-10 z-20">
-        <p className="text-[#ffffff93] font-bold text-[25px]">Eng sara kino va mulfilmlar TOP<span className="text-[orange]">Kino</span>.NET da</p>
+        <p className="text-[#ffffff93] font-bold text-[25px]">Eng sara kino va multfilmlar TOP<span className="text-[orange]">Kino</span>.NET da</p>
       </div>
       <div className=" absolute w-full top-0 left-0 bg-linear-to-t h-full from-black to-transparent z-10"></div>
     </div>
