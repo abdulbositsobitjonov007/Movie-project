b0664ee14326676aa838b027344bed4344c5f4c9
abdulbositsobitjonov007 <sobitjonovabdulbosit91@gmail.com>
2026-04-06 18:27:37 +0500

    saving changes

--- app/actors/[actorId]/page.tsx
+++ app/actors/[actorId]/page.tsx
@@ -1,4 +1,5 @@
-import { ActorType } from "@/types/ActorsType"
+import ActorSinglePage from "@/components/ActorSinglePage"
+
 
 const ActorSingle = async ({
     params
@@ -6,7 +7,7 @@ const ActorSingle = async ({
     params: { actorId: string }
 }) => {
 
-    const { actorId } = params
+    const { actorId } = await params;
 
     const res = await fetch(
         `https://x8ki-letl-twmt.n7.xano.io/api:j6hO02gL/actor/${actorId}`,
@@ -16,21 +17,11 @@ const ActorSingle = async ({
     const actorData = await res.json()
     console.log(actorData);
 
-    const allActors = await fetch(
-        `https://x8ki-letl-twmt.n7.xano.io/api:j6hO02gL/actor`,
-        { next: { revalidate: 3600 } }
-    )
-
-    const allActorsData = await allActors.json()
-    console.log(allActorsData);
-
-    const actor = allActorsData?.find((el: ActorType) => el.id === actorId)
-    console.log(actor);
 
     return (
         <div className="container mx-auto">
             <div>
-                page
+                <ActorSinglePage actor={actorData} />
             </div>
         </div>
     )
